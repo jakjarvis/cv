2544a1b8de61fef3f3d29116b08ddcca8709be97
Jono <67866962+jakjarvis@users.noreply.github.com>
2021-05-06 13:52:46 +0200

    Final menu updates

--- 3engineers/resources/js/script.js
+++ 3engineers/resources/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     
-    /* navigation scroll*/
+    /* navigation scroll */
     $(function(){
          $('a[href*=#]:not([href=#])').click(function() {
             if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
@@ -28,6 +28,49 @@ $(document).ready(function() {
         .fadeIn(1000)
         .end()
         .appendTo(".slider");
-    }, 3000);
+    }, 3000);  
+    
+    /* Mobile nav */
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        
+        nav.slideToggle(200);
+        if (icon.hasClass('fa-bars')) {
+            icon.addClass('fa-times-circle-o');
+            icon.removeClass('fa-bars');
+        } else {
+            icon.addClass('fa-bars');
+            icon.removeClass('fa-times-circle-o');
+        }
+    });
    
+    
+    /* Sticky nav */  
+    $('.js--section-about').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else { 
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px;'
+    });
+    
+    $(window).resize(function(){
+
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        
+        if ($(window).width() > 767){
+            nav.css("display", "block");
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            nav.css("display", "none");
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    });
+    
 });
